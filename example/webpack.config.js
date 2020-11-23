42325c44c99e014f79f2bc74c15b1c038727516b
bzztbomb <skinny@knowhere.net>
2020-11-22 19:31:39 -0800

    Split example code out to a separate project

---
+++ example/webpack.config.js
@@ -0,0 +1,46 @@
+const { resolve } = require('path');
+
+const { CheckerPlugin } = require('awesome-typescript-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'dist'),
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+
+  devtool: 'source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['awesome-typescript-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  plugins: [
+    new CheckerPlugin(),
+    new ProgressBarPlugin(),
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/src/index.html`,
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
+};
